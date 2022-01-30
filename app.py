413898dabfca0bb3e53a0f50b982010b7ed97898
Bristol Nimerik <dinalo@Bristols-MacBook-Pro.local>
2022-01-31 00:43:40 +0100

    added color for selected apps

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import tkinter as tk
-from tkinter import Frame, filedialog, Text
+from tkinter import filedialog, Text
 import os
 
 # holds the whole app
 root = tk.Tk()
 root.configure(bg="#EAE9E9")
-root.title(f"R.Y.D")
-# append the files 
+# append the files
 apps = []
 
-# load up the tex file 
+# load up the tex file
 if os.path.isfile('save.txt'):
     with open('save.txt', 'r') as f:
         tempApps = f.read()
@@ -17,21 +16,23 @@ if os.path.isfile('save.txt'):
         apps = [x for x in tempApps if x.strip()]
 
 
-# function to add exe files (for windows)  
+# function to add exe files (for windows)
 def addApp():
 
     # to update the apps on the screen
     for widget in frame.winfo_children():
         widget.destroy()
-
-    filename = filedialog.askopenfilename(initialdir="/", title="select file", filetypes=(("executables", "*.exe"), ("all files", "*.*"))) # also gives the location of the picked file 
+    filename = filedialog.askopenfilename(initialdir="/", title="select file", filetypes=(
+        ("executables", "*.exe"), ("all files", "*.*")))  # also gives the location of the picked file
 
     apps.append(filename)
     print(filename)
     for app in apps:
-        label = tk.Label(frame, text=app, bg="#000000")
+        label = tk.Label(frame, text=app, bg="black")
 
-# run the app 
+# run the app
+
+
 def runApps():
     for app in apps:
         os.startfile(app)
@@ -46,13 +47,15 @@ canvas.pack()
 frame = tk.Frame(root, bg="#EAE9E9")
 frame.place(relwidth=0.8, relheight=0.7, relx=0.1, rely=0.1)
 
-# buttons 
-# open files 
-openFile = tk.Button(root, text="open File", padx=20, pady=5, fg="#DEEEEA", bg="#ef6c26", command=addApp)
+# buttons
+# open files
+openFile = tk.Button(root, text="open File", padx=20, pady=5,
+                     fg="black", bg="orange", command=addApp)
 openFile.pack()
 
-# run apps 
-runApps = tk.Button(root, text="run Apps", padx=20, pady=5, fg="#DEEEEA", bg="#ef6c26", command=runApps)
+# run
+runApps = tk.Button(root, text="run Apps", padx=20, pady=5,
+                    fg="black", bg="orange", command=runApps)
 runApps.pack()
 
 # populate the saved apps on the screen
@@ -65,16 +68,14 @@ for app in apps:
 root.mainloop()
 
 
-
 # create a file and save the apps to stay when the machine is shuted down
 with open("save.txt", "w") as f:
     for app in apps:
         f.write(app + ',')
 
 
-# Todo: 
-# test it on windows vm 
-# add a functionallity for delete the apps of single apps from the list 
-# make runnable for unix systems 
-# find a better design of the app 
-
+# Todo:
+# test it on windows vm
+# add a functionallity for delete the apps of single apps from the list
+# make runnable for unix systems
+# find a better design of the app
